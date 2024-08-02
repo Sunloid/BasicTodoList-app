5624b114cd45e41459052c4e5525ff2b502eb198
Sunloid <ahmedrizavi04@gmail.com>
2024-08-02 21:38:15 +0530

    The code is still incomplete

--- App.js
+++ App.js
@@ -3,8 +3,39 @@ import { StyleSheet, Text, View } from "react-native";
 import Task from "./components/Task";
 import { KeyboardAvoidingView } from "react-native";
 import Input from "./components/input";
+import Constants from "expo-constants";
+import { useState } from "react";
+import {
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([])
+  const handleAddTask = () => {
+    setTaskItems([...taskItems, task])
+    setTask(null)}
+  function Yes() {
+    if (Constants.platform.ios) {
+      return (
+        <View style={styles.writeTaskWrapper}>
+          <TextInput
+            style={styles.input}
+            placeholder={"Write it here"}
+            onChangeText={(task) => setTask(task)}
+            value={task}
+          />
+          <TouchableOpacity onPress={() => handleAddTask()}>
+            <View style={styles.addWrapper}>
+              <Text style={styles.addText}>+</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+  }
+  
   return (
     <View style={styles.container}>
       <View style={styles.tasksWrapper}>
@@ -18,7 +49,12 @@ export default function App() {
         </View>
       </View>
       <KeyboardAvoidingView style={styles.writeTaskWrapper}>
-        <Input></Input>
+        {
+          Yes()
+        }
+        <View>
+          <Text>HEllo world</Text>
+        </View>
       </KeyboardAvoidingView>
     </View>
   );
@@ -48,7 +84,38 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    zIndex: 1,
     bottom: 60,
+    backgroundColor:"#FF0000"
   },
-});
+  input: {
+    paddingVertical: 15,
+    width: "80%",
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: "1",
+    height: 50,
+  },
+  addWrapper: {
+    backgroundColor: "#FFF",
+    width: 60,
+    height: 50,
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    justifyContent: "center",
+    alignContent: "items",
+  },
+  addText: {
+    backgroundColor: "#FF0000",
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    zIndex: 1,
+  },
+})
